24e3d8c044e59e924b5e5e43e563e41f21b71556
Vincent Morrill <vmorrilll@gmail.com>
2025-07-16 18:04:41 -0500

    Add Cybernetic Wedge Trader mod

---
+++ CyberneticTrader/ZoneBuilders/CyberneticTraderInjector.cs
@@ -0,0 +1,24 @@
+using System;
+using XRL.World;
+using XRL.World.ZoneBuilders;
+
+namespace CyberneticTraderMod
+{
+    [Serializable]
+    public class CyberneticTraderInjector : ZoneBuilder
+    {
+        public override void BuildZone(Zone Z)
+        {
+            if (!Z.ZoneID.Contains("sixdaystilt"))
+                return;
+
+            // Avoid duplicate placement
+            if (Z.FindObject(o => o.Blueprint == "CyberneticWedgeTraderNPC") != null)
+                return;
+
+            GameObject trader = GameObjectFactory.Factory.CreateObject("CyberneticWedgeTraderNPC");
+            Cell cell = Z.GetCell(65, 17) ?? Z.GetFirstEmptyCell();
+            cell?.AddObject(trader);
+        }
+    }
+}
