b3dded3bf427a4062ce7cacac7201df959b576c5
Vincent Morrill <vmorrilll@gmail.com>
2025-07-16 19:01:10 -0500

    Fix trader injector return type and conversation attributes

--- CyberneticTrader/ZoneBuilders/CyberneticTraderInjector.cs
+++ CyberneticTrader/ZoneBuilders/CyberneticTraderInjector.cs
@@ -7,18 +7,19 @@ namespace CyberneticTraderMod
     [Serializable]
     public class CyberneticTraderInjector : ZoneBuilder
     {
-        public override void BuildZone(Zone Z)
+        public override bool BuildZone(Zone Z)
         {
             if (!Z.ZoneID.Contains("sixdaystilt"))
-                return;
+                return true;
 
             // Avoid duplicate placement
             if (Z.FindObject(o => o.Blueprint == "CyberneticWedgeTraderNPC") != null)
-                return;
+                return true;
 
             GameObject trader = GameObjectFactory.Factory.CreateObject("CyberneticWedgeTraderNPC");
-            Cell cell = Z.GetCell(65, 17) ?? Z.GetFirstEmptyCell();
+            Cell cell = Z.GetCell(65, 17) ?? Z.GetRandomEmptyCell();
             cell?.AddObject(trader);
+            return true;
         }
     }
 }
