23b04c7d0a94396a57b196af1f42b5cc5779b24c
Vincent Morrill <vmorrilll@gmail.com>
2025-07-16 19:31:11 -0500

    Fix conversation choice case and zone builder return

--- CyberneticTrader/ZoneBuilders/CyberneticTraderInjector.cs
+++ CyberneticTrader/ZoneBuilders/CyberneticTraderInjector.cs
@@ -7,19 +7,21 @@ namespace CyberneticTraderMod
     [Serializable]
     public class CyberneticTraderInjector : ZoneBuilder
     {
-        public override void BuildZone(Zone Z)
+        public override bool BuildZone(Zone Z)
         {
             if (!Z.ZoneID.Contains("sixdaystilt"))
-                return;
+                return false;
 
             // Avoid duplicate placement
             if (Z.FindObject(o => o.Blueprint == "CyberneticWedgeTraderNPC") != null)
-                return;
+                return false;
 
             GameObject trader = GameObjectFactory.Factory.CreateObject("CyberneticWedgeTraderNPC");
             Cell cell = Z.GetCell(65, 17);
             if (cell != null)
                 cell.AddObject(trader);
+
+            return true;
         }
     }
 }
